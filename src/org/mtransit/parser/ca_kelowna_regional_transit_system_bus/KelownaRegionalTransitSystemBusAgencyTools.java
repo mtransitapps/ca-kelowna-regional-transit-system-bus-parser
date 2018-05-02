0636e7427d99b4e801441c9a943f605b2a93aefb
Mathieu Méa <mathieu.mea@gmail.com>
2018-05-01 23:59:03 -0400

    Stop excluding services!

--- src/org/mtransit/parser/ca_kelowna_regional_transit_system_bus/KelownaRegionalTransitSystemBusAgencyTools.java
+++ src/org/mtransit/parser/ca_kelowna_regional_transit_system_bus/KelownaRegionalTransitSystemBusAgencyTools.java
@@ -53,7 +53,7 @@ public class KelownaRegionalTransitSystemBusAgencyTools extends DefaultAgencyToo
 				.printf("\nGenerating Kelowna Regional Transit System bus data... DONE in %s.\n", Utils.getPrettyDuration(System.currentTimeMillis() - start));
 	}
 
-	private static final String INCLUDE_ONLY_SERVICE_ID_CONTAINS = "KL";
+	private static final String INCLUDE_ONLY_SERVICE_ID_CONTAINS = null;
 
 	@Override
 	public boolean excludeCalendar(GCalendar gCalendar) {
@@ -220,7 +220,7 @@ public class KelownaRegionalTransitSystemBusAgencyTools extends DefaultAgencyToo
 	@Override
 	public int compareEarly(long routeId, List<MTripStop> list1, List<MTripStop> list2, MTripStop ts1, MTripStop ts2, GStop ts1GStop, GStop ts2GStop) {
 		if (ALL_ROUTE_TRIPS2.containsKey(routeId)) {
-			return ALL_ROUTE_TRIPS2.get(routeId).compare(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop);
+			return ALL_ROUTE_TRIPS2.get(routeId).compare(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop, this);
 		}
 		return super.compareEarly(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop);
 	}
@@ -236,7 +236,7 @@ public class KelownaRegionalTransitSystemBusAgencyTools extends DefaultAgencyToo
 	@Override
 	public Pair<Long[], Integer[]> splitTripStop(MRoute mRoute, GTrip gTrip, GTripStop gTripStop, ArrayList<MTrip> splitTrips, GSpec routeGTFS) {
 		if (ALL_ROUTE_TRIPS2.containsKey(mRoute.getId())) {
-			return SplitUtils.splitTripStop(mRoute, gTrip, gTripStop, routeGTFS, ALL_ROUTE_TRIPS2.get(mRoute.getId()));
+			return SplitUtils.splitTripStop(mRoute, gTrip, gTripStop, routeGTFS, ALL_ROUTE_TRIPS2.get(mRoute.getId()), this);
 		}
 		return super.splitTripStop(mRoute, gTrip, gTripStop, splitTrips, routeGTFS);
 	}
@@ -252,9 +252,6 @@ public class KelownaRegionalTransitSystemBusAgencyTools extends DefaultAgencyToo
 	@Override
 	public boolean mergeHeadsign(MTrip mTrip, MTrip mTripToMerge) {
 		if (!isGoodEnoughAccepted()) {
-			if (false) {
-				return false; // TODO clean this
-			}
 			System.out.printf("\nUnexpected trips to merge %s & %s!\n", mTrip, mTripToMerge);
 			System.exit(-1);
 			return false;
